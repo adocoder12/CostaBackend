9fcd52f7e1603f7a3a768aa5fbfdcb747c54ddf2
adocoder12 <adonay.jadal@gmail.com>
2026-05-09 15:38:51 +0300

    addi interface

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -1,4 +1,18 @@
 package repository
 
-type ApartmentsRepositoryInterface interface{}
+import (
+	"context"
+
+	"github.com/adocoder12/Costabackend/internal/model"
+	"github.com/google/uuid"
+)
+
+type ApartmentsRepositoryInterface interface {
+	GetAllApartments(ctx context.Context) ([]model.Apartment, error)
+	GetByEmail(ctx context.Context, email string) (*model.Apartment, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*model.Apartment, error)
+	Create(ctx context.Context, user *model.Apartment) (*model.Apartment, error)
+	Update(ctx context.Context, user *model.Apartment) (*model.Apartment, error)
+	Delete(ctx context.Context, id uint) error
+}
 type CleanersRepositoryInterface interface{}
